65fdf0f2538ba30b377578fb2701df495deb0f31
CrisGadea <cristianhernangadea@gmail.com>
2019-08-16 15:50:08 -0300

    Added first migration and modified app and UserController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,10 +8,14 @@ class UsersController extends Controller
 {
     function index()
     {
-        $user = new User();
+    /*    $user = new User();
         $user->name = 'Cristian';
         $user->email = 'cristian@test.com';
 
-        return response()->json([$user],200);
+        return response()->json([$user],200); */
+
+        // USAMOS ELOQUENT PARA LA BASE DE DATOS  (Descomentar en boostrap/App.php la línea de app->withEloquent)
+        $users = User::all();
+        return response()->json($users,200);
     }
 }
